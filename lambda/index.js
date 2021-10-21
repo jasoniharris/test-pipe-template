7ff4f7e9db492c51f751d8ac544d564faa8da62d
EC2 Default User <ec2-user@ip-172-31-10-19.eu-west-2.compute.internal>
2021-10-21 23:56:17 +0000

    Reverting

--- lambda/index.js
+++ lambda/index.js
@@ -21,7 +21,7 @@ const HelloWorldIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello Test!';
+        const speakOutput = 'Hello World!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
